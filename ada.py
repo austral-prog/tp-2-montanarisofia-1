4d24b22bc1419786b806dfb19835a2d3827a020f
montanarisofia <162723443+montanarisofia@users.noreply.github.com>
2024-08-20 00:01:04 -0300

    Add files via upload

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = first_name + " " + last_name
+    print(f"{full_name.lower()}\n{full_name.title()}\n{full_name.upper()}\n\t{full_name.lower()}")
+ada()
+
+
--- change.py
+++ change.py
@@ -1,3 +1,9 @@
 def change():
-    expense = 23.75
-    money = 100
+    spent= float(input("Ingresar gasto:\n"))
+    received= float(input("Dinero recibido:\n"))
+ #Calculating change from now change= received - spent
+    change= received-spent
+    intchange= int((change - int(change))*100)
+    print("\nVuelto")
+    print (f"\nPesos:\n{int(change)}\nCentavos:\n{intchange}")
+change()
