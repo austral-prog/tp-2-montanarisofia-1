1d8be135310d09a12ec50337b9e193ad5470ca6f
montanarisofia <162723443+montanarisofia@users.noreply.github.com>
2024-08-20 00:04:02 -0300

    ada.py

--- ada.py
+++ ada.py
@@ -3,6 +3,4 @@ def ada():
     last_name = "LoVeLAce"
     full_name = first_name + " " + last_name
     print(f"{full_name.lower()}\n{full_name.title()}\n{full_name.upper()}\n\t{full_name.lower()}")
-ada()
-
 
