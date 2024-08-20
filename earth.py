69bef1abf1474deea23d6a33325581b346a818eb
montanarisofia <162723443+montanarisofia@users.noreply.github.com>
2024-08-20 00:03:17 -0300

    earth.py

--- earth.py
+++ earth.py
@@ -2,4 +2,3 @@ def earth():
     x = "Bangladesh"
     y = "Barbados"
     print(f"The result of X comes first in the dictionary than Y is {x>y}.\nThe result of Y comes first in the dictionary than X is {y>x}.")
-earth()
