db75eef491e49bf105aeedd14a9ea47193e9ebfd
montanarisofia <162723443+montanarisofia@users.noreply.github.com>
2024-08-20 00:38:17 -0300

    change.py

--- change.py
+++ change.py
@@ -1,7 +1,6 @@
 def change():
     spent= float(input("Ingresar gasto:\n"))
     received= float(input("Dinero recibido:\n"))
- #Calculating change from now change= received - spent
     change= received-spent
     intchange= int((change - int(change))*100)
     print("\nVuelto")
